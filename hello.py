af71888101c9ac55370d0ea16e0d4977b588e493
Ubuntu <ubuntu@ip-10-210-0-30.ec2.internal>
2022-07-14 13:12:10 +0000

    update

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     print("hello")
-    return "Hello World!"
+    return "Thanks ML team for joining this ml session!"
 
 if __name__ == "__main__":
     app.run('0.0.0.0',9999)
