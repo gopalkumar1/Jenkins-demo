f806c1352a2da7269ac0b2eac39dd2cdd7868142
gopalkumar1 <52914965+gopalkumar1@users.noreply.github.com>
2022-07-15 16:40:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     print("hello")
-    return "hello! everyone"
+    return "Welcome"
 
 if __name__ == "__main__":
     app.run('0.0.0.0',9999)
