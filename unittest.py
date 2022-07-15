c6862244abea694eb07c6b2f4384e484edc1e953
gopalkumar1 <52914965+gopalkumar1@users.noreply.github.com>
2022-07-15 15:14:52 +0530

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -1,5 +1,5 @@
 try: 
-	from run import app
+	from hello import app
 	import unittest
 except Exception as e:
 	print("some error")
@@ -7,14 +7,15 @@ class FlaskTest(unittest.TestCase):
 	# check for response 200	
 	def test_index(self):
 		tester = app.test_client(self)
-		response = tester.get("/fo")
+		response = tester.get("/")
 		statuscode = response.status_code
 		self.assertEqual(statuscode, 200)
+		
 	# checl if content return is application/json
 	def test_index_content(self):
 		tester = app.test_client(self)
-		response = tester.get("/fo")
+		response = tester.get("/")
 		self.assertEqual(response.content_type,"application/json")
-		self.assertEqual(statuscode, 200)
+	
 if __name__ == "__main__":
 	unittest.main()
